cc7e1a5a64ac437d1e3559fed61eb781f84031c7
swlee <swo98@naver.com>
2025-10-20 18:01:18 +0900

    init: project init

---
+++ src/main/java/study/batchperformance/BatchPerformanceApplication.java
@@ -0,0 +1,13 @@
+package study.batchperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchPerformanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/batchperformance/BatchPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package study.batchperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPerformanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
